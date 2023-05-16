bbcffa8a9139b9fa31d57f7ae3ddebbc60b220aa
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-05-16 12:08:35 +0300

    Add project files.

---
+++ SocialBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
